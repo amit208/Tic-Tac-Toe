bb80d67fb1638c6873eaf7d9aa2cffc8758312a4
amit208 <amitkumar.ppt208@gmail.com>
2018-11-01 07:47:54 +0530

    Gave the cards a 3d flip animation and also add a semitransparent background for the cards

---
+++ js/brain.js
@@ -0,0 +1,35 @@
+// ===================================
+// This code will rotate the card if any of the buttons of the front card is clicked
+// ===================================
+const btnFriend = document.querySelector("#friend");
+const btnComputer = document.querySelector("#computer");
+const valueOfFrontCard;
+
+btnFriend.addEventListener("click", flipCard);
+btnComputer.addEventListener("click", flipCard);
+
+function flipCard(event) {
+    valueOfFrontCard = event.target;
+    document.querySelector(".cards").classList.toggle("is-flipped");
+}
+// =========================================
+
+
+
+// ===========================================
+// This code will close the card if any of the button of the back of the card is clicked
+// and will also remove the semiTransparent black background
+// ===========================================
+const btnCross = document.querySelector("#cross");
+const btnRing = document.querySelector("#ring");
+const valueOfBackCard;
+
+btnCross.addEventListener("click", closeCard);
+btnRing.addEventListener("click", closeCard);
+
+function closeCard(event) {
+    valueOfBackCard = event.target;
+    document.querySelector(".cards").style.display = "none";
+    document.querySelector(".backgroundForCards").classList.toggle("semiTransparentBg");
+}
+// ===========================================

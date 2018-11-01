fd171e503e0f13f6397d658826c85e13b5889611
amit208 <amitkumar.ppt208@gmail.com>
2018-11-01 13:32:32 +0530

    Made some change to the index.html and js file to reflect the player and player symbol according to the input in the starting modal

--- js/brain.js
+++ js/brain.js
@@ -3,17 +3,17 @@
 // ===================================
 const btnFriend = document.querySelector("#friend");
 const btnComputer = document.querySelector("#computer");
-let valueOfFrontCard;
+let valueOfFrontCard = "";
+
+function flipCard(event) {
+    valueOfFrontCard = event.target.value;
+
+    document.querySelector(".cards").classList.toggle("is-flipped");
+}
 
 btnFriend.addEventListener("click", flipCard);
 btnComputer.addEventListener("click", flipCard);
-
-function flipCard(event) {
-    valueOfFrontCard = event.target;
-    document.querySelector(".cards").classList.toggle("is-flipped");
-}
 // =========================================
-
 
 // ===========================================
 // This code will close the card if any of the button of the back of the card is clicked
@@ -22,13 +22,30 @@ function flipCard(event) {
 const btnCross = document.querySelector("#cross");
 const btnRing = document.querySelector("#ring");
 let valueOfBackCard;
-
-btnCross.addEventListener("click", closeCard);
-btnRing.addEventListener("click", closeCard);
+let symbol = ["O", "X"];
+let symbolOfPlayer1;    //Player1 will always be the user
+let symbolOfPlayer2;    //player2 will always be the second player or computer
 
 function closeCard(event) {
-    valueOfBackCard = event.target;
+    valueOfBackCard = event.target.value;
+    symbolOfPlayer1 = symbol[valueOfBackCard];
+
+    let indexOfRestSymbol = 1 - valueOfBackCard;
+    symbolOfPlayer2 = symbol[indexOfRestSymbol];
+    
+    //This code will change the player name according to the buttons clicked on the first card
+    if (valueOfFrontCard === "friend") {
+        document.querySelector("#player1 p").innerHTML = `Player - ${symbolOfPlayer1}`;
+        document.querySelector("#player2 p").innerHTML = `Player - ${symbolOfPlayer2}`;
+    } else {
+        document.querySelector("#player1 p").innerHTML = `Player - ${symbolOfPlayer1}`;
+        document.querySelector("#player2 p").innerHTML = `Computer - ${symbolOfPlayer2}`;
+    }
+
     document.querySelector(".cards").style.display = "none";
     document.querySelector(".backgroundForCards").classList.toggle("semiTransparentBg");
 }
+
+btnCross.addEventListener("click", closeCard);
+btnRing.addEventListener("click", closeCard);
 // ===========================================
